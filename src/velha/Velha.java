96c5f9792ab555086c58e59bebbfe40efc9c13ee
LeonardoGarciah <leonardoformulaum@Hotmail.com>
2021-09-29 15:46:38 -0300

    Added Project

---
+++ src/velha/Velha.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package velha;
+
+/**
+ *
+ * @author leona
+ */
+public class Velha {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        aplicacao ap = new aplicacao();
+        ap.setResizable(false);
+        ap.setVisible(true);
+        ap.setLocationRelativeTo(null);
+        
+    }
+    
+}
